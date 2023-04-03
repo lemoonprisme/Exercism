c8611a7b1c01f44785199a2368ff37a7640c57dd
Talanov Maxim <t33maks33@gmail.com>
2023-04-03 15:09:46 +0300

    Added IfStatements.Triangle.csproj

---
+++ IfStatements.Triangle/Triangle.cs
@@ -0,0 +1,30 @@
+﻿namespace IfStatements.Triangle;
+
+public static class Triangle
+{
+    public static bool IsTriangle(double a, double b, double c)
+        {
+            return (a + b) >= c && (b + c) >= a && (a + c) >= b && (a != 0 || b != 0 || c != 0);
+        }
+    
+        public static bool IsScalene(double side1, double side2, double side3)
+        {
+            if(Triangle.IsTriangle(side1, side2, side3)) 
+                return side1 != side2 && side2 != side3 && side1 != side3;
+            return false;
+        }
+    
+        public static bool IsIsosceles(double side1, double side2, double side3) 
+        {
+            if(Triangle.IsTriangle(side1, side2, side3)) 
+                return side1 == side2 || side2 == side3 || side1 == side3;
+            return false;
+        }
+    
+        public static bool IsEquilateral(double side1, double side2, double side3) 
+        {
+            if(Triangle.IsTriangle(side1, side2, side3)) 
+                return side1 == side2 && side2 == side3;
+            return false;
+        }
+}
